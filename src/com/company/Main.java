a452db56960b54ef066d0bddeb710d6b3911ba58
hz-98 <2601065004@qq.com>
2019-09-27 14:07:48 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    private static String java;
+
+    public Main() {
+        java = "java";
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+
+        String java = Main.java;
+    }
+}
